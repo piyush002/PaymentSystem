cbe93829e59f1fd8b0a7476f2823ed5458cfd426
build-break <build.break2018@gmail.com>
2018-10-13 19:16:03 +0530

    payment System application

---
+++ src/main/java/com/piyush/banking/PaymentSystemApplication.java
@@ -0,0 +1,12 @@
+package com.piyush.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemApplication.class, args);
+	}
+}
