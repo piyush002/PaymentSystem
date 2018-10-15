2d94d921e9dded428365ed9c5b77dfdc6c188f37
piyush002 <piyushm0072@gmail.com>
2018-10-15 11:24:33 +0200

    Update TransferServiceImplTest.java
    updated exception test cases

--- src/test/java/com/piyush/banking/service/TransferServiceImplTest.java
+++ src/test/java/com/piyush/banking/service/TransferServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.piyush.banking.service;
 
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -20,33 +21,109 @@ import com.piyush.banking.exception.InsufficientBalanceException;
 @RunWith(MockitoJUnitRunner.class)
 public class TransferServiceImplTest {
 
-  @InjectMocks
-  TransferServiceImpl transferServiceImpl;
+	@InjectMocks
+	TransferServiceImpl transferServiceImpl;
 
-  @Mock
-  AccountRepository accountRepository;
+	@Mock
+	AccountRepository accountRepository;
 
-  @Test
-  public void transferTest() throws EntityNotFoundException, InsufficientBalanceException {
-    Long testAccountNumber = 1L;
-    Account toAccount = new Account();
-    toAccount.setAccountNumber(testAccountNumber);
-    toAccount.setBalance(5000);
-    toAccount.setName("Piyush");
-    Account fromAccount = new Account();
-    fromAccount.setAccountNumber(testAccountNumber);
-    fromAccount.setBalance(2300);
-    fromAccount.setName("Uygar");
-    TransferDTO transferDTO = new TransferDTO();
-    transferDTO.setAmount(123.0);
-    transferDTO.setFromAccount(6L);
-    transferDTO.setToAccount(1L);
+	@Test
+	public void transferTest() throws EntityNotFoundException, InsufficientBalanceException {
+		Long testAccountNumber = 1L;
+		Account toAccount = new Account();
+		toAccount.setAccountNumber(testAccountNumber);
+		toAccount.setBalance(5000);
+		toAccount.setName("Piyush");
+		Account fromAccount = new Account();
+		fromAccount.setAccountNumber(testAccountNumber);
+		fromAccount.setBalance(2300);
+		fromAccount.setName("Uygar");
+		TransferDTO transferDTO = new TransferDTO();
+		transferDTO.setAmount(123.0);
+		transferDTO.setFromAccount(6L);
+		transferDTO.setToAccount(1L);
 
-    Optional<Account> fromAccountdetails = Optional.of(fromAccount);
-    Optional<Account> toAccountdetails = Optional.of(toAccount);
-    when(accountRepository.findById(transferDTO.getFromAccount())).thenReturn(fromAccountdetails);
-    when(accountRepository.findById(transferDTO.getToAccount())).thenReturn(toAccountdetails);
-    transferServiceImpl.transfer(transferDTO);
+		Optional<Account> fromAccountdetails = Optional.of(fromAccount);
+		Optional<Account> toAccountdetails = Optional.of(toAccount);
+		when(accountRepository.findById(transferDTO.getFromAccount())).thenReturn(fromAccountdetails);
+		when(accountRepository.findById(transferDTO.getToAccount())).thenReturn(toAccountdetails);
+		transferServiceImpl.transfer(transferDTO);
+	}
 
-  }
+	@Test(expected = InsufficientBalanceException.class)
+	public void transferTestInsufficientBalance() throws InsufficientBalanceException {
+		Long testAccountNumber = 1L;
+		Account toAccount = new Account();
+		toAccount.setAccountNumber(testAccountNumber);
+		toAccount.setBalance(5000);
+		toAccount.setName("Piyush");
+		Account fromAccount = new Account();
+		fromAccount.setAccountNumber(testAccountNumber);
+		fromAccount.setBalance(300);
+		fromAccount.setName("Uygar");
+		TransferDTO transferDTO = new TransferDTO();
+		transferDTO.setAmount(350.0);
+		transferDTO.setFromAccount(6L);
+		transferDTO.setToAccount(1L);
+
+		Optional<Account> fromAccountdetails = Optional.of(fromAccount);
+		when(accountRepository.findById(transferDTO.getFromAccount())).thenReturn(fromAccountdetails);
+		try {
+			transferServiceImpl.transfer(transferDTO);
+		} catch (EntityNotFoundException e) {
+			fail("Exception Should not be thrown");
+		} catch (InsufficientBalanceException e) {
+			throw new InsufficientBalanceException("Insufficient account balance ");
+		}
+	}
+
+	@Test(expected = EntityNotFoundException.class)
+	public void transferTestFromAccountNotFound() throws EntityNotFoundException {
+		Long testAccountNumber = 1L;
+		Account toAccount = new Account();
+		toAccount.setAccountNumber(testAccountNumber);
+		toAccount.setBalance(5000);
+		toAccount.setName("Piyush");
+		TransferDTO transferDTO = new TransferDTO();
+		transferDTO.setAmount(50.0);
+		transferDTO.setFromAccount(6L);
+		transferDTO.setToAccount(1L);
+		Optional<Account> fromAccountdetails = Optional.empty();
+		when(accountRepository.findById(transferDTO.getFromAccount())).thenReturn(fromAccountdetails);
+		try {
+			transferServiceImpl.transfer(transferDTO);
+		} catch (EntityNotFoundException e) {
+			throw new EntityNotFoundException("Account Details not found");
+		} catch (InsufficientBalanceException e) {
+			fail("Exception Should not be thrown");
+		}
+	}
+
+	@Test(expected = EntityNotFoundException.class)
+	public void transferTestToAccountNotFound() throws EntityNotFoundException {
+		Long testAccountNumber = 1L;
+		Account toAccount = new Account();
+		toAccount.setAccountNumber(testAccountNumber);
+		toAccount.setBalance(5000);
+		toAccount.setName("Piyush");
+		Account fromAccount = new Account();
+		fromAccount.setAccountNumber(testAccountNumber);
+		fromAccount.setBalance(300);
+		fromAccount.setName("Uygar");
+		TransferDTO transferDTO = new TransferDTO();
+		transferDTO.setAmount(50.0);
+		transferDTO.setFromAccount(6L);
+		transferDTO.setToAccount(1L);
+		Optional<Account> fromAccountdetails = Optional.of(fromAccount);
+		Optional<Account> toAccountdetails = Optional.empty();
+		when(accountRepository.findById(transferDTO.getFromAccount())).thenReturn(fromAccountdetails);
+		when(accountRepository.findById(transferDTO.getToAccount())).thenReturn(toAccountdetails);
+		try {
+			transferServiceImpl.transfer(transferDTO);
+		} catch (EntityNotFoundException e) {
+			throw new EntityNotFoundException("Account Details not found");
+		} catch (InsufficientBalanceException e) {
+			fail("Exception Should not be thrown");
+		}
+	}
 }
